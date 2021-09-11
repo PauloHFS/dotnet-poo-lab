edb93330d77719420fdb92ed408a0124be76ad04
Paulo Hernane <paulo.hernane.silva@ccc.ufcg.edu.br>
2021-09-11 09:12:07 -0300

    Parte 3

---
+++ DIO.Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DIO.Series/Interfaces/IRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DIO.Series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorID(int id);
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
